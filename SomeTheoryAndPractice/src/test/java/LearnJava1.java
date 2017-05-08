43b668df66e4393d6ccede45b453009dcc9a7873
Sergii Kavun <vid4inyay@gmail.com>
2017-05-08 22:44:21 +0200

    New learning directory added

---
+++ SomeTheoryAndPractice/src/test/java/LearnJava1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by vid4i on 08.05.2017.
+ */
+public class LearnJava1 {
+    // declare primitive variables
+    private int variable1 = 1;
+    private float variable2 = 2.2f;
+
+
+    // declare reference variables
+
+
+}
