4881c51875dec4fa513b9ae9e98e73e650f60851
Sergii Kavun <vid4inyay@gmail.com>
2017-05-08 23:10:24 +0200

    New learning folder and files

---
+++ SomeTheoryAndPractice/src/test/java/JavaBasics1.java
@@ -0,0 +1,27 @@
+/**
+ * Created by vid4i on 08.05.2017.
+ */
+public class JavaBasics1 {
+
+    // declare primitive variables
+    private int variable1 = 1;
+    private float variable2 = 2.2f;
+    private double var2 = -1.23;
+    private char variable3 = '/';
+    private char var4 = 'c';
+
+    //  Class Math contains all math operations
+
+    // Math random returns double
+    private double someRandom =  Math.random();
+
+
+
+    // declare reference variables
+
+
+
+
+    public static void main(String[] args) {
+    }
+}
